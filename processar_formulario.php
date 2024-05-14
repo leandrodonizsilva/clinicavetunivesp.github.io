e397f72830d42ddbd8e38a4ab4b49ec91b4e7577
leandrodonizsilva <152231324+leandrodonizsilva@users.noreply.github.com>
2024-05-14 17:11:05 -0300

    Add files via upload

---
+++ processar_formulario.php
@@ -0,0 +1,29 @@
+<?php
+$servername = "Leandro";
+$username = "root@localhost";
+$password = "univesp";
+$dbname = "veterinaria";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Falha na conexão com o banco de dados: " . $conn->connect_error);
+}
+
+$nomeCliente = $_POST['nomeCliente'];
+$endereco = $_POST['endereco'];
+$nomeAnimal = $_POST['nomeAnimal'];
+$raca = $_POST['raca'];
+$idAnimal = $_POST['idAnimal'];
+
+$sql = "INSERT INTO clientes (nome_cliente, endereco, nome_animal, raca, id_animal)
+        VALUES ('$nomeCliente', '$endereco', '$nomeAnimal', '$raca', '$idAnimal')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Registro inserido com sucesso!";
+} else {
+    echo "Erro ao inserir registro: " . $conn->error;
+}
+
+$conn->close();
+?>
